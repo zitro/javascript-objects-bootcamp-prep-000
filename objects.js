23d7a2b2062285411c8952b047217e9d4335e996
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 18:10:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,8 +9,8 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 }
 
-//function removeFromPlaylist(playlist, artistName){
+function removeFromPlaylist(playlist, artistName){
 
-//Delete playlist.artistName;
+delete playlist.artistName;
 
-//}
+}
